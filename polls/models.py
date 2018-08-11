e65dc35fc56009e7161f6c3dd0f4f747367bf1f1
Rajan Khullar <rkhullar03@gmail.com>
2018-08-11 17:27:48 -0400

    added simple methods to poll models

--- polls/models.py
+++ polls/models.py
@@ -2,13 +2,25 @@ from django.db.models import CharField, DateTimeField, IntegerField, ForeignKey
 from django.db.models import Model
 from django.db import models
 
+from django.utils import timezone
+import datetime as dt
+
 
 class Question(Model):
     question_text = CharField(max_length=200)
     pub_date = DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self) -> bool:
+        return self.pub_date >= timezone.now() - dt.timedelta(days=1)
 
 
 class Choice(Model):
     question = ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = CharField(max_length=200)
     votes = IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
