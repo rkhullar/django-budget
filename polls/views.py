cd22d6c570606db25740f186d248e625686cf1f3
Rajan Khullar <rkhullar03@gmail.com>
2018-08-11 21:00:00 -0400

    cleaning up old views

--- polls/views.py
+++ polls/views.py
@@ -6,24 +6,6 @@ from django.views import generic
 from .models import Question, Choice
 
 
-'''
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = dict(latest_question_list=latest_question_list)
-    return render(request, 'polls/index.html', context)
-
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', dict(question=question))
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', dict(question=question))
-'''
-
-
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
