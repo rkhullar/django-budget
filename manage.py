d9a947e9bcc5edfc78b0c2c3e938164f43dfde1c
Rajan Khullar <rkhullar03@gmail.com>
2018-10-07 18:21:59 -0400

    added helper to load env variables from local config

--- manage.py
+++ manage.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python
-import os
+from configparser import RawConfigParser
+from pathlib import Path
 import sys
+import os
+
+
+def iter_config(path: Path):
+
+    if path.exists():
+        config = RawConfigParser()
+        with path.open('r') as f:
+            config.read_file(f)
+
+        for section in config.sections():
+            for key, val in config[section].items():
+                yield key, val
+
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nyit.settings')
+    to_read = Path(__file__).parent / 'local' / 'config.cfg'
+    for key, val in iter_config(to_read):
+        os.environ.setdefault(key.upper(), val)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
